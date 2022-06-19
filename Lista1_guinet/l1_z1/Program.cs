91f4ff899d86e59ae72debc39dffe64a4a9d6466
Paweł Jurkiw <58842005+jasivo@users.noreply.github.com>
2022-06-19 10:06:18 +0200

    Add files via upload

---
+++ Lista1_guinet/l1_z1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Podaj liczbe: ");
+int number = int.Parse(Console.ReadLine());
+for(int i=0; i<=number; i++)
+{
+    Console.Write(i + "\t");
+    for(int j=1; j<=number; j++)
+    {
+            if(i == 0)
+            {
+                Console.Write("{0,4}", j + "\t");
+            }
+            else
+            {
+                    Console.Write("{0,4}", (i*j)+ "\t");
+            }
+    }
+    Console.Write("\n");
+}
---
+++ Lista1_guinet/l1_z2/Program.cs
@@ -0,0 +1,65 @@
+﻿Calkowite a = new Calkowite(5);
+a.WypiszStan();
+Console.WriteLine(a.Wallet);
+a.WypiszStan();
+Console.WriteLine(a.Wallet = 2500);
+Console.WriteLine(a.Wallet);
+a.WypiszStan();
+Console.WriteLine(a.Wallet);
+Console.WriteLine(a.Wallet);
+Console.WriteLine(a.Wallet);
+Console.WriteLine(a.Wallet);
+a.WypiszStan();
+a.Ureguluj();
+a.WypiszStan();
+
+class Calkowite
+{
+    private int _freeOperations;
+    private int _wallet = 0;
+    private int _doneRead = 0;
+    private int _doneWrite = 0;
+
+    public Calkowite(int operations)
+    {
+        this._freeOperations = operations;
+    }
+
+    public int Wallet
+    {
+        get
+        {
+            if(_doneRead < _freeOperations)
+            {
+                this._doneRead++;
+                return _wallet;
+            }
+            else
+            {
+                Console.WriteLine("Wykorzystales wszystkie bezplatne operacje pobrania.");
+                return -1;
+            }
+        }
+        set
+        {
+            if(_doneWrite < _freeOperations)
+            {
+                this._doneWrite++;
+                this._wallet = value;
+            }
+            else
+                Console.WriteLine("Wykorzystales wszystkie bezplatne operacje nadania.");
+        }
+    }
+
+    public void Ureguluj()
+    {
+        this._doneRead = 0;
+        this._doneWrite = 0;
+    }
+
+    public void WypiszStan()
+    {
+        Console.WriteLine("Pozostało " + (_freeOperations - _doneRead) + " bezpłatnych operacji pobrania i "+ (_freeOperations-_doneWrite) + " operacji nadania.");
+    }
+}
