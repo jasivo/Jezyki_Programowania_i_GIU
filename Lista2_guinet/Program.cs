16c632016e714f49e64df7497bc24271f71c2100
Paweł Jurkiw <58842005+jasivo@users.noreply.github.com>
2022-06-19 10:08:45 +0200

    Add files via upload

---
+++ Lista2_guinet/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+
+    Lista lista0 = new Lista(10);
+    Console.WriteLine("Lista0 metoda ToString:");
+    Console.WriteLine(lista0);
+    Console.WriteLine(" ");
+
+    List<Lista1> lista1 = new List<Lista1>();
+    for(int i = 0; i < 5; i++)
+    {
+        Lista1 x = new Lista1(); 
+        lista1.Add(x);
+    }
+    Console.WriteLine("Lista1 przed sortowaniem");
+    lista1.ForEach(l => Console.WriteLine(l));
+    Console.WriteLine(" ");
+    lista1.Sort();
+    Console.WriteLine("Lista1 po sortowaniu");
+    lista1.ForEach(l => Console.WriteLine(l));
+    Console.WriteLine(" ");
+
+    List<Lista2> lista2 = new List<Lista2>();
+    for (int i = 0; i < 5; i++)
+    {
+        Lista2 y = new Lista2();
+        lista2.Add(y);
+    }
+    Console.WriteLine("Lista2 przed sortowaniem");
+    lista2.ForEach(l => Console.WriteLine(l));
+    Console.WriteLine(" ");
+    lista2.Sort();
+    Console.WriteLine("Lista2 po sortowaniu");
+    lista2.ForEach(l => Console.WriteLine(l));
+    Console.WriteLine(" ");
+
+class Lista
+{
+    public List<int> _lista = new List<int>();
+    public Random rand = new Random();
+
+    public Lista(int n)
+    {    
+        if(n>=0)
+        {    
+            for(int i=0; i<n; i++)
+                this._lista.Add(rand.Next(1, 101));
+        }
+        else
+            Console.WriteLine("Podaj nieujemną długość listy!");
+    }
+
+    public Lista()
+    {
+        for(int i=0; i<rand.Next(1,6); i++)
+        {
+            this._lista.Add(rand.Next(1, 101));
+        }
+    }
+
+    public override String ToString()
+    {
+        return String.Join(" ",_lista);
+    }
+
+}
+
+class Lista1 : Lista, IComparable<Lista1>
+{
+    public Lista1() : base() { } 
+    public Lista1(int n) : base(n) { } 
+
+public int CompareTo(Lista1 compareList)
+    {
+        //dlugosc krotszej listy
+        int _minimum;
+        if(this._lista.Count > compareList._lista.Count) 
+            _minimum = compareList._lista.Count; 
+        else
+            _minimum = this._lista.Count; 
+
+        if (this._lista.Count == 0 && compareList._lista.Count == 0)
+            return 0;
+        else if (this._lista.Count == 0)
+            return -1;
+        else if (compareList._lista.Count == 0) 
+            return 1;
+           
+        for(int i = 0; i < _minimum; i++)
+        {
+            if(this._lista[i] < compareList._lista[i])
+                return -1;
+            else if (this._lista[i] > compareList._lista[i])
+                return 1;
+        }
+
+        if (this._lista.Count < compareList._lista.Count) 
+            return -1;
+        else if(this._lista.Count > compareList._lista.Count)
+            return 1;
+        return 0;
+    }
+}
+
+class Lista2 : Lista, IComparable<Lista2>
+{
+    public Lista2() : base() { }
+    public Lista2(int n) : base(n) { }
+
+    public int CompareTo(Lista2 compareList)
+    {
+        if (this._lista.Count == 0 && compareList._lista.Count == 0)
+            return 0;
+        else if (this._lista.Count == 0)
+            return -1;
+        else if (compareList._lista.Count == 0)
+            return 1;
+
+        if (this._lista.Count < compareList._lista.Count)
+            return -1;
+        else if (this._lista.Count > compareList._lista.Count)
+            return 1;
+
+        for (int i = 0; i < this._lista.Count; i++)
+        {
+            if (this._lista[i] < compareList._lista[i])
+                return -1;
+            else if (this._lista[i] > compareList._lista[i])
+                return 1;
+        }
+        return 0;
+    }
+}
---
+++ Lista2_guinet/zadanie2/Program.cs
@@ -0,0 +1,52 @@
+﻿//Program wylicza srednia ocen szkoly na podstawie ocen ze wszystkich klas.
+
+Srednia szkoly = new Srednia();
+
+Console.WriteLine(szkoly.policz_srednia().ToString("0.00"));
+Console.Write("\n");
+
+class Srednia
+{
+    public Random rand;
+    public int _klasy;
+    public int[][] _oceny;
+    public float n;
+    public float suma;
+
+    public Srednia()
+    {
+        rand = new Random();
+        _klasy = rand.Next(1,9);
+        _oceny = new int[_klasy][];
+        n = 0;
+        suma = 0;
+
+        for(int i = 0; i < _klasy; i++)
+        {
+            int _rozmiar = rand.Next(10,20);
+            int[] _srednia = new int[_rozmiar];
+            for(int j = 0; j < _rozmiar; j++)
+            {
+                _srednia[j] = rand.Next(1,7);
+            }
+            _oceny[i] = _srednia;
+        }
+    }
+
+    public float policz_srednia()
+    {
+        for(int i = 0 ; i < _oceny.Length; i++)
+        {
+            for(int j = 0; j < _oceny[i].Length; j++)
+            {
+                this.suma += _oceny[i][j];
+                this.n++;
+                Console.Write(_oceny[i][j]);
+                Console.Write(" ");
+            }
+        }
+        Console.Write("\nSrednia szkoly = ");
+
+        return (this.suma/this.n);
+    }
+}
